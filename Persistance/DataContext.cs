fd46418b3fd893e70ac15a36e6acffbf272b3fd1
Michael Walker <michaelwalker846@yahoo.com>
2021-06-05 20:58:00 -0400

    Chapter 19 - Implemented SignalR for live chat

--- Persistance/DataContext.cs
+++ Persistance/DataContext.cs
@@ -16,6 +16,8 @@ namespace Persistance
 
         public DbSet<Photo> Photos { get; set; }
 
+        public DbSet<Comment> Comments { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -25,6 +27,8 @@ namespace Persistance
             builder.Entity<ActivityAttendee>().HasOne(u => u.AppUser).WithMany(a => a.Activities).HasForeignKey(aa => aa.AppUserId);
 
             builder.Entity<ActivityAttendee>().HasOne(u => u.Activity).WithMany(a => a.Attendees).HasForeignKey(aa => aa.ActivityId);
+
+            builder.Entity<Comment>().HasOne(a => a.Activity).WithMany(c => c.Comments).OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
